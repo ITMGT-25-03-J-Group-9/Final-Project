feb899978d0a7c5a5b1789b36579ebd2ba3eb67a
unknown <nyle.que@student.ateneo.edu>
2024-07-20 19:22:56 +0800

    attempted to add size button

--- ashop_root/ashop/core/admin.py
+++ ashop_root/ashop/core/admin.py
@@ -1,31 +1,39 @@
 from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path
-from .models import Product, CartItem, Transaction, LineItem
+from .models import Product, CartItem, Transaction, LineItem, Size
 from .forms import ProductForm
 
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
-	list_display = ('name', 'size', 'price', 'quantity', 'image')
-	fields = ('name', 'size', 'price', 'quantity', 'image')  
+	list_display = ('name', 'display_sizes', 'price', 'quantity', 'image')
+	fields = ('name', 'size', 'price', 'quantity', 'image',) 
 	form = ProductForm
-	search_fields = ('name',)
-	list_filter = ('price', 'size')
+	search_fields = ('name',) 
+	list_filter = ('price',)
+
+	def display_sizes(self, obj):
+		return ", ".join([size.size_name for size in obj.size.all()])		
+	display_sizes.short_description = 'Sizes'
 
 class CartItemAdmin(admin.ModelAdmin):
-	list_display = ('user', 'product', 'quantity')
-	list_filter = ('user', 'product')
+	list_display = ('user', 'product', 'size', 'quantity')
+	list_filter = ('user', 'product', 'size')
 
 class TransactionAdmin(admin.ModelAdmin):
 	list_display = ('user', 'created_at')
 	list_filter = ('user', 'created_at')
 
 class LineItemAdmin(admin.ModelAdmin):
-	list_display = ('transaction', 'product', 'quantity')
-	list_filter = ('transaction', 'product')
+	list_display = ('transaction', 'product', 'size', 'quantity')
+	list_filter = ('transaction', 'product', 'size')
+
+class SizeAdmin(admin.ModelAdmin):
+	list_display = ('size_code', 'size_name')
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(CartItem, CartItemAdmin)
 admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(LineItem, LineItemAdmin)
+admin.site.register(Size, SizeAdmin)
--- ashop_root/ashop/core/models.py
+++ ashop_root/ashop/core/models.py
@@ -3,25 +3,33 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Product(models.Model):
+class Size(models.Model):
 	SIZE_CHOICES = [
 		('S', 'Small'),
 		('M', 'Medium'),
 		('L', 'Large'),
 	]
 
+	size_code = models.CharField(max_length=1, choices=SIZE_CHOICES, unique=True)
+	size_name = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return self.size_name
+
+class Product(models.Model):
 	name = models.CharField(max_length=50)
 	price = models.IntegerField()
 	image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 	quantity = models.PositiveIntegerField(default=0)
-	size = models.CharFieldsize = models.CharField(max_length=1, choices=SIZE_CHOICES, default='M')
+	size = models.ManyToManyField(Size, related_name='products')
 
 	def __str__(self):
-		return f'{self.name} ({self.get_size_display()})'
+		return self.name
 
 class CartItem(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 	product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+	size = models.ForeignKey(Size, on_delete=models.CASCADE, null=False)
 	quantity = models.IntegerField()
 	def __str__(self):
 		return f'{self.quantity} of {self.product} (User: {self.user.username})'
@@ -46,6 +54,7 @@ class Transaction(models.Model):
 class LineItem(models.Model):
 	transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, null=False)
 	product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+	size = models.ForeignKey(Size, on_delete=models.CASCADE, null=False) 
 	quantity = models.IntegerField()
 
 	def __str__(self):
